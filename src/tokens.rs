920a1a590447ca61d1afaf359b2c590284f4bc53
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-12-15 15:27:38 -0500

    combined tokens into single file

---
+++ src/tokens.rs
@@ -0,0 +1,88 @@
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Operator {
+    DECREMENT,
+    NEGATION,
+    BITWISENOT,
+}
+
+impl FromStr for Operator {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "--" => Ok(Operator::DECREMENT),
+            "!" => Ok(Operator::NEGATION),
+            "~" => Ok(Operator::BITWISENOT),
+            _ => Err(format!("Unknown operator: {}", s)),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Keyword {
+    INT,
+    VOID,
+    RETURN,
+}
+
+impl FromStr for Keyword {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "int" => Ok(Keyword::INT),
+            "void" => Ok(Keyword::VOID),
+            "return" => Ok(Keyword::RETURN),
+            _ => Err(format!("Unknown keyword: {}", s)),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Delimiter {
+    LPAREN,
+    RPAREN,
+    SEMICOLON,
+    LBRACE,
+    RBRACE,
+}
+
+impl FromStr for Delimiter {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "(" => Ok(Delimiter::LPAREN),
+            ")" => Ok(Delimiter::RPAREN),
+            ";" => Ok(Delimiter::SEMICOLON),
+            "{" => Ok(Delimiter::LBRACE),
+            "}" => Ok(Delimiter::RBRACE),
+            _ => Err(format!("Unknown delimiter: {}", s)),
+        }
+    }
+}
+
+/// Tokens in C source code.
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
+    CONSTANT(i64),
+    KEYWORD(Keyword),
+    IDENTIFIER(String),
+    OPERATOR(Operator),
+    DELIMITER(Delimiter),
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Token::CONSTANT(value) => write!(f, "CONSTANT({})", value),
+            Token::KEYWORD(kw) => write!(f, "KEYWORD({:?})", kw),
+            Token::IDENTIFIER(name) => write!(f, "IDENTIFIER({})", name),
+            Token::OPERATOR(op) => write!(f, "OPERATOR({:?})", op),
+            Token::DELIMITER(delim) => write!(f, "DELIMITER({:?})", delim),
+        }
+    }
+}
